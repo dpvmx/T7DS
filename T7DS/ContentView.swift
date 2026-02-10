127b253acde6327f20355fd9dfb5f58743f34f0f
Damián Paz <talcko@gmail.com>
2026-02-10 17:46:03 -0600

    Initial Commit

---
+++ T7DS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  T7DS
+//
+//  Created by tests on 10/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ T7DS/T7DSApp.swift
@@ -0,0 +1,17 @@
+//
+//  T7DSApp.swift
+//  T7DS
+//
+//  Created by tests on 10/02/26.
+//
+
+import SwiftUI
+
+@main
+struct T7DSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
